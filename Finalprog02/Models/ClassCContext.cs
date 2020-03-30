6cc1834808a21f9ebb5d97fa25bf82ec5ab2a5dc
Leyms <leyra017@hotmail.com>
2020-03-30 18:23:14 -0400

    Creada la Clase DbContext completa

---
+++ Finalprog02/Models/ClassCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Finalprog02.Models
+{
+    public class ClassCContext:DbContext
+    {
+        public DbSet<ClassPacientes> Pacientes { get; set; }
+        public DbSet<ClassMedicos> Medicos { get; set; }
+        public DbSet<ClassHabitaciones> Habitaciones { get; set; }
+        public DbSet<ClassIngresos> Ingresos { get; set; }
+        public DbSet<ClassCitas> Citas { get; set; }
+        public DbSet<ClassAltas> Altas { get; set; }
+
+    }
+}
