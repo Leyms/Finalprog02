840aec0e0a211650b8338fd1b08839e61e86316f
Leyms <leyra017@hotmail.com>
2020-03-30 19:02:28 -0400

    Creadas vistas mantenimiento y procesos ademas el routeconfi

--- Finalprog02/App_Start/RouteConfig.cs
+++ Finalprog02/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Finalprog02
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "OpcionesGenerales", id = UrlParameter.Optional }
             );
         }
     }
