d45b5c6b3f20006efeb2c81435d09a034dfa0216
Leyms <leyra017@hotmail.com>
2020-03-31 00:59:36 -0400

    PDF en Altas Medicas Listo

--- Finalprog02/Controllers/AltasController.cs
+++ Finalprog02/Controllers/AltasController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Finalprog02.Models;
+using Rotativa;
 
 namespace Finalprog02.Controllers
 {
@@ -19,6 +20,11 @@ namespace Finalprog02.Controllers
         {
             var altas = db.Altas.Include(c => c.Ingresos);
             return View(altas.ToList());
+        }
+        public ActionResult imprimir()
+        {
+            var print = new ActionAsPdf("Index");
+            return print;
         }
 
         [HttpPost]
