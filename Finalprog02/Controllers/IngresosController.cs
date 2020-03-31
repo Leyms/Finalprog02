261ed12532a1713a63b52f5e9ad1c7b9cce2145c
Leyms <leyra017@hotmail.com>
2020-03-31 00:52:39 -0400

    Listo procedimiento de PDF En Ingresos

--- Finalprog02/Controllers/IngresosController.cs
+++ Finalprog02/Controllers/IngresosController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Finalprog02.Models;
+using Rotativa;
 
 namespace Finalprog02.Controllers
 {
@@ -19,6 +20,11 @@ namespace Finalprog02.Controllers
         {
             var ingresos = db.Ingresos.Include(c => c.Habitacion).Include(c => c.Pacientes);
             return View(ingresos.ToList());
+        }
+        public ActionResult imprimir()
+        {
+            var print = new ActionAsPdf("Index");
+            return print;
         }
         [HttpPost]
         public ActionResult Index(string busqueda, string select)
